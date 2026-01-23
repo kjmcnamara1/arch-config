9c057d8d7272bcf7239922d0f607b3e026c889ea
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2026-01-23 14:17:23 -0500

    feat(module): add neovim

---
+++ modules/neovim/dotfiles/nvim/init.lua
@@ -0,0 +1,14 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+require("core.options")
+
+vim.pack.add("https://github.com/rmehri01/onenord.nvim")
+require("onenord").setup(
+  {
+    styles = {
+      comments = "italic",
+      keywords = "bold",
+    },
+  }
+)
